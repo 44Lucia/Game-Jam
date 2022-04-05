4ce3301b660750ebe8bff99af1508a6ac333dbba
Lucia Fornos Balaguer <95747188+44Lucia@users.noreply.github.com>
2022-04-05 11:17:58 +0200

    Cambios menus

---
+++ Assets/Scripts/Lucia/MenuLoos.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLoos : MonoBehaviour
+{
+    string scene_name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene escena = SceneManager.GetActiveScene();
+        scene_name = escena.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toMainMenu() 
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void toGame() 
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
